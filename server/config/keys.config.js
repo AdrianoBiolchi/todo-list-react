7316c1630a4e8688c833da9894220eb95d93bed2
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-04-16 22:01:11 -0300

    new package.json

---
+++ server/config/keys.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodbUri: 'mongodb+srv://todo-user:user-password@cluster0-mw6v9.mongodb.net/test?retryWrites=true'
+}
--- server/server.js
+++ server/server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const TaskRoutes = require('./routes/Task');
+const mongoose = require('mongoose');
+const config = require('./config/keys.config.js');
 
 
 app.use(bodyParser.json());
 app.use('/api/tasks', TaskRoutes);
+
+mongoose.connect(config.mongodbUri, { useNewUrlParser: true })
+  .then(() => console.log('Database conectada'))
+  .catch((err) => console.log('Erro ao se conectar a database', err));
+mongoose.set('useFindAndModify', false);
 
 
 const port = process.env.PORT || 3001;
